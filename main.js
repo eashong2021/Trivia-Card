495149f764d9d0429b31f737f7246cdaa207a06e
Ebenezer Ashong <ebenezer.ashong@amalitech.org>
2021-11-16 22:37:09 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(() =>{
+    //click Event
+  $('.hint-box').on('click', () => {
+   $('.hint') .slideToggle(800);
+  });
+  // Wrong Answers
+  $('.wrong-answer-one').on('click', () => {
+  $('.wrong-text-one').fadeOut('slow');
+  $('.frown').show();
+  });
+  
+  $('.wrong-answer-two').on('click', () => {
+  $('.wrong-text-two').fadeOut('fast');
+  $('.frown').show();
+  });
+  
+  $('.wrong-answer-three').on('click', () => {
+  $('.wrong-text-three').fadeOut();
+  $('.frown').show();
+  });
+  
+  //Correct Answer
+  $('.correct-answer').on('click', () => {
+  $('.frown').hide();
+  $('.smiley').show();
+  $('.wrong-text-one').fadeOut('slow');
+  $('.wrong-text-two').fadeOut('slow');
+  $('.wrong-text-three').fadeOut('slow');
+  });
+  });
